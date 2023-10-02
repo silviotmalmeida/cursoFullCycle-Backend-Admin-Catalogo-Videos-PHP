5ec4a718096bf26aa7f8a800bce8015fd90ff1d9
Silvio Almeida <silviotmalmeida@gmail.com>
2023-10-02 20:57:21 -0300

    Ajustes gerais

---
+++ app/src/Core/Domain/Validation/DomainValidation.php
@@ -0,0 +1,18 @@
+<?php
+
+// definindo o namespace, referente ao caminho das pastas
+namespace Core\Domain\Validation;
+
+// importações
+use Core\Domain\Entity\Traits\MagicMethodsTrait;
+use Core\Domain\Exception\EntityValidationException;
+
+// definindo a classe
+// classe com  métodos genéricos de validação
+class DomainValidation
+{
+    
+    public static function notNull(string $value, string $exceptMessage = null){
+
+    }
+}

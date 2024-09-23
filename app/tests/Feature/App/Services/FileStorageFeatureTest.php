72576727820f68a459bff3de8a607fe84192c699
Silvio Almeida <silviotmalmeida@gmail.com>
2024-09-23 09:15:33 -0300

    Ajustes gerais

--- app/tests/Feature/App/Services/FileStorageFeatureTest.php
+++ app/tests/Feature/App/Services/FileStorageFeatureTest.php
@@ -3,8 +3,6 @@
 namespace Tests\Feature\App\Services;
 
 use App\Services\Storage\FileStorage;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
@@ -17,6 +15,30 @@ class FileStorageFeatureTest extends TestCase
      * @return void
      */
     public function test_store_delete()
+    {
+        // criando arquivo fake
+        $fakeFile = UploadedFile::fake()->create('video.mp4', 1, 'video/mp4');
+
+        // carregando os dados do fakeFile em um array $_FILES padrão php
+        $file = [
+            'name' => $fakeFile->getFilename(),
+            'type' => $fakeFile->getMimeType(),
+            'tmp_name' => $fakeFile->getPathname(),
+            'error' => $fakeFile->getError(),
+            'size' => $fakeFile->getSize(),
+        ];
+
+        // armazenando o arquivo
+        $filePath = (new FileStorage())->store('videos', $file);
+
+        // verificando
+        Storage::assertExists($filePath);
+
+        // apagando o arquivo
+        Storage::delete($filePath);
+    }
+
+    public function test_delete()
     {
         // criando arquivo fake
         $fakeFile = UploadedFile::fake()->create('video.mp4', 1, 'video/mp4');
